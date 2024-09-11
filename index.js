21696a7f31128eb826600b81927030f51dff369f
idevRavn <idevravn@gmail.com>
2024-09-11 22:13:59 +0530

    Add display book function

--- index.js
+++ index.js
@@ -8,3 +8,27 @@ function Book(title, author, pages, read) {
   this.pages = pages;
   this.read = read;
 }
+
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+}
+
+book1 = new Book("The Hobbit", "J.R.R. Tolkien", 295, true);
+addBookToLibrary(book1);
+console.log(myLibrary);
+
+function displayBooks() {
+  myLibrary.forEach((book) => {
+    const cardDiv = document.createElement("div");
+    cardDiv.classList.add("book-card");
+
+    const title = document.createElement("h3");
+    title.textContent = book.title;
+    title.classList.add("title");
+    cardDiv.appendChild(title);
+
+    booksContainer.appendChild(cardDiv);
+  });
+}
+
+displayBooks();
